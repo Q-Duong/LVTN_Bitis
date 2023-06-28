67d1e56c28607a1ccf290d99213276664f6cc59f
Long2109-tran <93022910+Long2109-tran@users.noreply.github.com>
2023-06-28 08:35:07 +0700

    search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\Banner;
 use App\Models\Post;
+use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
@@ -17,4 +18,36 @@ class HomeController extends Controller
     public function wistlist(){
        return view('pages.wistlist.wistlist');
    }
+   public function search(Request $request){
+        $keywords = $request->keywords_submit;
+        $product = Product::where('product_name','like','%'.$keywords.'%')->get();
+        return view('pages.product.search')->with(compact('keywords','product'));
+   }
+   public function search_autocomplete(Request $request){
+        $data = $request->all();
+        if($data['query']){
+            $product = Product::where('product_name','like','%'.$data['query'].'%')->limit(8)->get(); 
+            $output = '
+            <ul  class="dropdown-menu">
+                <li class="li_search_ajax">Gợi ý tìm kiếm</li>'
+            ;
+
+            foreach($product as $key => $val){
+               $output .= '
+               <li class="li_search_ajax"><a href="'.url('/products/'.$val->product_slug).'" ><i class="fas fa-search"></i>'.$val->product_name.'</a></li>
+               ';
+            }
+
+            $output .= '</ul>';
+            echo $output;
+        }else{
+            $output = '
+            <ul  class="dropdown-menu">
+                <li class="li_search_ajax">Gợi ý tìm kiếm</li>'
+            ;
+            $output .= '</ul>';
+            echo $output;
+        }
+   }
+
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -500,7 +500,7 @@
                 var _token = $('input[name="_token"]').val();
 
                 $.ajax({
-                    url: "{{ url('/autocomplete-ajax') }}",
+                    url: "{{ url('/search-autocomplete') }}",
                     method: "POST",
                     data: {
                         query: query,
