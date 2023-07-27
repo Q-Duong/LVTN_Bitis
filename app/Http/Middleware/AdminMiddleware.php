dbd6620e0acdfc04c93264cc788493a448af561f
minhanzenc <93908719+minhanzenc@users.noreply.github.com>
2023-07-28 00:02:56 +0700

    update 403

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -25,7 +25,6 @@ class AdminMiddleware
                 return Redirect()->route('member/login');
             }
         }else{
-            dd(1);
             return Redirect()->route('login');
         }
     }
--- resources/views/403.blade.php
+++ resources/views/403.blade.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Bạn không đủ quyền để truy cập</h2>
-</body>
-</html>
+@extends('admin_layout')
+@section('admin_content')
+    
+<div class="row">
+    <div class="col-lg-12">
+        <section class="panel">
+            <div class="panel-body">
+                <div class="position-center">
+                        <div class="form-group ">
+                            <h2 class="panel-heading">Bạn không đủ quyền để truy cập</h2>
+                        </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
